ecd5cc225c91c00c939f4b4570bdc1fbcb896c92
patryk-baczek <84987929+patryk-baczek@users.noreply.github.com>
2022-03-07 21:15:22 +0100

    gallery js

---
+++ js/gallery.js
@@ -0,0 +1,10 @@
+let slider = tns({
+    container: '.gallery-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    mode:'gallery',
+    speed:800,
+    controlsPosition:'top',
+    navPosition:'bottom'
+  });
